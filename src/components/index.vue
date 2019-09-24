c98dd12ce8d437e24c5ada29d25b786da9b52d82
ryanrocket <ryan.wans@icloud.com>
2019-09-23 20:34:52 -0400

    fixed issue :P

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,9 +1,15 @@
 <!-- Downstream Count: 3 FINAL -->
 
 <template>
-  <div class="index" >
-    <h1 class="hMotd" data-aos="fade-left" data-aos-duration="1100">{{ motd }}<br><b id="nolight">Ryan Wans</b>
-    <br><br><p id="de" class="floating" owner>{{ jtitle }}</p></h1>
+  <div class="index">
+    <h1 class="hMotd" data-aos="fade-left" data-aos-duration="1100">
+      {{ motd }}
+      <br />
+      <b id="nolight">Ryan Wans</b>
+      <br />
+      <br />
+      <p id="de" class="floating" owner>{{ jtitle }}</p>
+    </h1>
   </div>
 </template>
 
@@ -12,13 +18,11 @@ export default {
   name: 'index',
   props: ['motd', 'jtitle'],
 };
-(window.location.href === '/')
-(console['log']("Rendered Index Page Successfully");)
-(console['warn']("An Internal Warning Occured");)
+console.log('Rendered Index Page Successfully');
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-@import '../app.scss';
-@import '../compile.scss';
+@import "../app.scss";
+@import "../compile.scss";
 </style>
